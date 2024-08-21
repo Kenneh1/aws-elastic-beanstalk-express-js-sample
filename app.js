28de808376a643a5d4f2da23ae13baab9ce9af4c
unknown <kennehlosine90@gmail.com>
2024-08-22 02:17:27 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('welcome to kenneh project!'));
+app.get('/', (req, res) => res.send('welcome to kenneh project!We have added a review stage with manual approval to our continuous delivery pipeline '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
